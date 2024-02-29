7974c656b76e52d9061dd79849a007f21210d556
BlasEzequiel <blasespinazo@gmail.com>
2024-02-28 23:23:01 -0300

    Mi primer Commit

---
+++ src/main/java/com/EGG/ServiSalud/ServiSaludApplication.java
@@ -0,0 +1,13 @@
+package com.EGG.ServiSalud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiSaludApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiSaludApplication.class, args);
+	}
+
+}
