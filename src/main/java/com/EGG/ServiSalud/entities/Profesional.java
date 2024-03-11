52fc8eeb65262649bb224db732c655591e4e8cf2
mufa94 <manuva_99@utp.edu.co>
2024-03-10 22:54:16 -0500

    creación de la entidad paciente con su persistencia, con su clase servicio con su controlador.

--- src/main/java/com/EGG/ServiSalud/entities/Profesional.java
+++ src/main/java/com/EGG/ServiSalud/entities/Profesional.java
@@ -1,9 +1,17 @@
 package com.EGG.ServiSalud.entities;
 
 import com.EGG.ServiSalud.entities.Enums.Especialidad;
+import jakarta.persistence.*;
 
+
+@Entity
+@Table(name = "profesionales")
 
 public class Profesional {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name ="idProfesional")
+    private Long idProfesional;
     private Especialidad especialidad;
     private Double reputacion;
     private Double valorConsultaConCS;
